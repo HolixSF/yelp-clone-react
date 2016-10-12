5bc6c1d577c3df33d8498adbf1c92c97bd8be202
Luis De Castro <luisdecastro510@gmail.com>
2016-10-12 15:32:18 -0700

    abstract routes to own file

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory, Router, Route } from 'react-router'
+import { browserHistory } from 'react-router'
 
 import 'font-awesome/css/font-awesome.css'
 import './app.css'
 
 import App from './containers/App/App'
+import makeRoutes from './routes'
 
-class Home extends React.Component {
-  render () {
-    return <div>Hello World</div>
-  }
-}
-
-const routes = (
-  <Router>
-    <Route path='/' component={Home} />
-  </Router>
-)
+const routes = makeRoutes()
 
 render(<App history={browserHistory} routes={routes} />, document.getElementById('root'))
