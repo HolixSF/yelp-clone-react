d8327ce73fd43dbd4d59ef6e7a040c69840fffd8
Luis De Castro <luisdecastro510@gmail.com>
2016-10-12 16:47:31 -0700

    add google maps react and setup api

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Map, { GoogleApiWrapper } from 'google-maps-react'
 
-export default class Container extends React.Component {
+export class Container extends React.Component {
   render () {
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    }
+
     return (
-      <div>
-        Hello from the container
+      <div style={style}>
+        <Map google={this.props.google} />
       </div>
     )
   }
 }
+
+export default GoogleApiWrapper({
+  apiKey: __GAPI_KEY__
+})(Container)
