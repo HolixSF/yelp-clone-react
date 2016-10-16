fff7c9e396c9315a4423dbba78b4e2eb3bdfde4c
Luis De Castro <luisdecastro510@gmail.com>
2016-10-16 15:30:23 -0700

    add detail component file and getdetails function

--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -3,12 +3,13 @@ import { Route } from 'react-router'
 
 import Container from './Container'
 import Map from './Map/Map'
+// import Detail from
 
 const makeMainRoutes = () => {
   return (
     <Route path='/' component={Container}>
       <Route path='map' component={Map} />
-      <Route path='detail/:placeId' component={Detail} />
+      {/* <Route path='detail/:placeId' component={Detail} /> */}
     </Route>
   )
 }
