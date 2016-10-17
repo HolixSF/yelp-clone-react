f8a7511372e31eb6ddc2087a84bc5f26b33c69ad
Luis De Castro <luisdecastro510@gmail.com>
2016-10-16 18:26:18 -0700

    add on click function to sidebar and children

--- src/components/Listing/Item.js
+++ src/components/Listing/Item.js
@@ -5,10 +5,20 @@ import Rating from 'components/Rating/Rating'
 import styles from './styles.module.css'
 
 export default class Item extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onClick(e) {
+    this.props.onClick(this.props.place)
+  }
+
   render () {
     const {place} = this.props
     return (
-      <div className={styles.item}>
+      <div onClick={this.onClick} className={styles.item}>
         <h1 className={classnames(styles.title)}>{place.name}</h1>
         <Rating className={styles.rating} percentage={(place.rating / 5)} />
       </div>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,13 +4,20 @@ import Listing from 'components/Listing/Listing'
 import styles from './styles.module.css'
 
 export default class Sidebar extends React.Component {
+  onClick (place, map, google) {
+    if (this.props.onListItemClick) {
+      place.place = place
+      this.props.onListItemClick(place, map, google)
+    }
+  }
+
   render () {
     return (
       <div className={styles.sidebar}>
         <div className={styles.heading}>
           <h1>{this.props.title}</h1>
         </div>
-        <Listing places={this.props.places} />
+        <Listing onClick={this.onClick.bind(this)} places={this.props.places} />
       </div>
     )
   }
